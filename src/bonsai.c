d2f79d83e46eac6d5251e65d53a32d5e72d5d7fd
billyg <overtaker_ric@qq.com>
2022-01-26 12:56:55 +0800

    update

--- src/bonsai.c
+++ src/bonsai.c
@@ -204,7 +204,7 @@ int bonsai_scan(pkey_t low, pkey_t high, pval_t* val_arr) {
 void bonsai_deinit() {
 	bonsai_print("bonsai deinit\n");
 
-	dump_pnode_list();
+	// dump_pnode_list();
 	
 	bonsai_self_thread_exit();
 
