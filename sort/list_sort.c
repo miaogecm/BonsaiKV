d0e871218131705c52e96a6210827e77cd78bb35
hhusjr <gnu_emacs@hhu.edu.cn>
2022-03-06 17:54:50 +0800

    update

--- sort/list_sort.c
+++ sort/list_sort.c
@@ -127,7 +127,7 @@ void list_sort(void *priv, struct list_head *head,
 		}
 		if (lev > max_lev) {
 			if (unlikely(lev >= ARRAY_SIZE(part)-1)) {
-				bonsai_print("list too long for efficiency\n");
+				printf("list too long for efficiency\n");
 				lev--;
 			}
 			max_lev = lev;
