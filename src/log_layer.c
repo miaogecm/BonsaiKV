0a0ffe1ec675be2d8ea5483ed2e5da309d2f0d4b
miaogecm <mcai@hhu.edu.cn>
2022-04-28 20:04:55 +0800

    update

--- src/log_layer.c
+++ src/log_layer.c
@@ -559,8 +559,7 @@ static void merge_and_cluster_logs(struct flush_load *per_socket_loads, logs_t *
     }
 
     if (unlikely(!total)) {
-		pthread_barrier_wait(barrier);
-		return;
+		goto out;
     }
 
     for (i = 0; i < NUM_SOCKET; i++) {
@@ -591,6 +590,7 @@ static void merge_and_cluster_logs(struct flush_load *per_socket_loads, logs_t *
         }
     }
 
+out:
 	pthread_barrier_wait(barrier);
 
 	free(logs[wid].logs);
@@ -629,7 +629,6 @@ static void inter_worker_cluster(struct flush_load *per_socket_loads,
         if (likely(!list_empty(target) && !list_empty(addon))) {
             prev = list_last_entry(target, struct cluster, list);
             curr = list_first_entry(addon, struct cluster, list);
-			printf("prev %016lx curr %016lx addon %016lx %016lx %016lx\n", prev, curr, addon, &addon->next, addon->next);
             if (prev->pnode == curr->pnode) {
                 pbatch_list_merge(&prev->pbatch_list, &curr->pbatch_list);
                 list_del(&curr->list);
