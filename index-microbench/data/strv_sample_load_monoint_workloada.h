e2656984c6fac63fb989c10a748dd37db7842c0a
billyg <overtaker_ric@qq.com>
2022-05-27 15:29:53 +0800

    updaet

--- index-microbench/data/strv_sample_load_monoint_workloada.h
+++
@@ -1,2 +0,0 @@
-#include "strv_sample_load_monoint_workloada.h_key"
-#include "strv_sample_load_monoint_workloada.h_val"
--- index-microbench/data/strv_sample_op_monoint_workloada.h
+++
@@ -1,3 +0,0 @@
-#include "strv_sample_op_monoint_workloada.h_key"
-#include "strv_sample_op_monoint_workloada.h_val"
-#include "strv_sample_op_monoint_workloada.h_type"
