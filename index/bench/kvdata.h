5b77357d4e5da980295f97495d6cff1010d9c26e
hhusjr <gnu_emacs@163.com>
2022-06-30 09:27:01 +0800

    update

--- index/bench/kvdata.h
+++ index/bench/kvdata.h
@@ -1,8 +1,8 @@
 // BEGIN kvdata header
 #include <stdint.h>
-extern uint64_t load_k_arr[1000000];
-extern char load_v_arr[1000000][16];
-extern uint8_t type_arr[1000000];
-extern uint64_t op_k_arr[1000000];
-extern char op_v_arr[1000000][16];
+extern char load_k_arr[36000000][16];
+extern uint64_t load_v_arr[36000000];
+extern uint8_t type_arr[36000000];
+extern char op_k_arr[36000000][16];
+extern uint64_t op_v_arr[36000000];
 // END kvdata header
