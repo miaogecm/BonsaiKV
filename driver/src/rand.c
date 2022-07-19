df3d3d8aedc111a802156f9c83bc6e26f1b83c26
miaogecm <445386319@qq.com>
2022-07-19 12:46:18 +0800

    driver

---
+++ driver/src/rand.c
@@ -0,0 +1,17 @@
+#include <time.h>
+#include <rand.h>
+
+static __thread unsigned int seed;
+static __thread int seeded = 0;
+
+static inline void thread_rand_init() {
+    seed = time(NULL);
+}
+
+int gen_rand() {
+    if (!seeded) {
+        thread_rand_init();
+        seeded = 1;
+    }
+    return rand_r(&seed);
+}
