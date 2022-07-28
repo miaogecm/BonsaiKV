3ad778d28c1218e1f0ea7bb0e77233f7f2cee640
hhusjr <gnu_emacs@hhu.edu.cn>
2022-07-29 00:32:43 +0800

    update

--- evaluation/kvstore/src/main.c
+++ evaluation/kvstore/src/main.c
@@ -3,5 +3,8 @@ void run_tpcc(const char *kvlib, int num_w_, int num_work_);
 void run_ycsb(const char *kvlib, int str_key_, int str_val_);
 
 int main() {
-    run_ycsb("../../src/libbonsai.so", 0, 0);
+    //run_ycsb("lib/listdb/cmake-build-release/liblistdb_dll.so", 0, 0);
+    //run_ycsb("../../src/libbonsai.so", 0, 0);
+    //run_ycsb("lib/pactree/cmake-build-release/src/libpactree.so", 0, 0);
+    run_ycsb("lib/fastfair/libfastfair.so", 0, 0);
 }
