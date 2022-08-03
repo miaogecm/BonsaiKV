839dd0391b129c21f0736d99cc546209098d6ece
hhusjr <gnu_emacs@163.com>
2022-08-03 11:07:12 +0800

    256->248

--- evaluation/kvstore/src/ycsb.c
+++ evaluation/kvstore/src/ycsb.c
@@ -16,7 +16,7 @@ static ycsb_decompressor_t load_dec, op_dec;
 #define LOAD_PATH   "tools/index-microbench/workloads/load"
 #define OP_PATH     "tools/index-microbench/workloads/op"
 
-#define VAL_LEN     256
+#define VAL_LEN     248
 static char valbuf[VAL_LEN];
 
 static void *get_val(size_t *len) {
