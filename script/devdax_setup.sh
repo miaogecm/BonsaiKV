9dc450afa8a31efb5f1fd9cbba962eaeaec3701b
hhusjr <gnu_emacs@163.com>
2022-08-05 15:38:08 +0800

    test

--- script/devdax_setup.sh
+++ script/devdax_setup.sh
@@ -21,7 +21,7 @@ for i in $(seq 0 $((NR_DIMM - 1))); do
     output=$(ndctl create-namespace --size=$size --region=region$i --mode=devdax | tee /dev/tty)
     dev=$(echo "$output" | python3 -c "import sys, json; print(json.load(sys.stdin)['daxregion']['devices'][0]['chardev'])")
     echo "/mnt/ext4/dimm$i/$filename -> /dev/$dev" >> .mapping
-    ln -s /dev/$dev /mnt/ext4/dimm$i
+    ln -s /dev/$dev /mnt/ext4/dimm$i/$filename
   done
 done
 
