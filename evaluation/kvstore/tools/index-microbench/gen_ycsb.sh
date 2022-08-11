91bb309a0aef91f643db41ce026d59a9cf711508
hhusjr <gnu_emacs@163.com>
2022-08-11 13:46:04 +0800

    update

--- evaluation/kvstore/tools/index-microbench/gen_ycsb.sh
+++ evaluation/kvstore/tools/index-microbench/gen_ycsb.sh
@@ -13,11 +13,17 @@ THREADS=(1 16 32 48 64 80 96)
 for config in "${CONFIGS[@]}"; do
   name=$(echo $config | awk "{ print $1 }")
   workload=$(echo $config | awk "{ print $2 }")
-  load_cnt=$(echo $config | awk "{ print $3 }")
-  op_cnt=$(echo $config | awk "{ print $4 }")
+  thread_load_cnt=$(echo $config | awk "{ print $3 }")
+  thread_op_cnt=$(echo $config | awk "{ print $4 }")
   distrib=$(echo $config | awk "{ print $5 }")
   str_key=$(echo $config | awk "{ print $6 }")
   for thread in "${THREADS}"; do
-
+    load_cnt=$((thread_load_cnt * thread))
+    op_cnt=$((thread_op_cnt * thread))
+    dir=workloads/$name/$thread
+    mkdir -p $dir
+    ./gen_workload.sh $workload $load_cnt $op_cnt $distrib
+    ../../compress $dir/load workloads/workload_load $str_key
+    ../../compress $dir/op workloads/workload_op $str_key
   done
 done
