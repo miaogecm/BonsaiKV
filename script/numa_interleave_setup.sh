b29f6c45bbfc894bb057d9729d6921d26bf10346
hhusjr <gnu_emacs@163.com>
2022-08-12 09:05:30 +0800

    scripts

--- script/numa_interleave_setup.sh
+++ script/numa_interleave_setup.sh
@@ -2,4 +2,13 @@
 
 ./dimm_interleave_setup.sh
 
+umount /mnt/ext4/*
+
 echo -e "0 $(( `blockdev --getsz /dev/mapper/interleaved-set0` * 2 )) striped 2 2048 /dev/mapper/interleaved-set0 0 /dev/mapper/interleaved-set1 0" | sudo dmsetup create --force numa-interleaved-set
+
+mkfs.ext4 /dev/mapper/numa-interleaved-set
+
+mount -t ext4 -o dax /dev/mapper/numa-interleaved-set /mnt/ext4/dimm0
+
+chown -R deadpool /mnt/ext4/*
+chgrp -R deadpool /mnt/ext4/*
