78ead5d4c8a0391a70234bd2e1037dacc75564eb
hhusjr <gnu_emacs@163.com>
2022-08-17 14:46:22 +0800

    ycsb_a_str

--- evaluation/kvstore/data/final/ycsb_a_str.py
+++ evaluation/kvstore/data/final/ycsb_a_str.py
@@ -8,31 +8,33 @@
 # val: 16KB
 # distribution: uniform
 # fsdax + dm-stripe (for NUMA-oblivious designs)
+# Enable snoopy mode for AD
 # measure: throughput(thread)
+
+# *** VIPER: SIGBUS ***
 
 M = 1000000
 K = 1000
 MAX = 5760 * K
 SIZE = 240 * K
-THREADS = [1, 8, 16, 24, 32, 40, 48]
+THREADS = [1, 6, 12, 18, 24, 30, 36, 42, 48]
 LATENCY = {
-    # thread num: 1/8/16/24/32/40/48
+    # thread num: 1/6/12/18/24/30/36/42/48
 
     # 1GB memtable, max number=4
     # Enabled 1GB lookup cache (979 MB hash-based, 45 MB second chance)
     # Nbg:Nfg = 1:2, at least 1 Nbg
-    'listdb':   [6.159, 10.293, 11.202, 12.699, 14.107, 17.873, 20.764],
+    'listdb':   [MAX, MAX, MAX, MAX, MAX, MAX, MAX, MAX, MAX],
 
     # dm-stripe 2M-Interleave
     # LOG_BATCHING enabled, simulates FlatStore log batching (batch size: 512B)
-    'pacman':   [2.156, 4.324, 4.640, 4.969, 8.090, 22.547, 48.724],
+    'pacman':   [1.767, 2.250, 3.441, 5.159, 6.898, 23.629, 28.833, 38.268, 43.489],
 
-    # dm-stripe 4K-Interleave
-    # bottleneck: VPage metadata cacheline thrashing, segment lock overhead, NUMA Awareness
-    'viper':    [3.393, 5.005, 4.984, 6.044, 6.980, 7.737, 10.433],
+    # dm-stripe 2M-Interleave
+    'viper':    [MAX, MAX, MAX, MAX, MAX, MAX, MAX, MAX, MAX],
 
     # Nbg:Nfg = 1:4, at least 2 Nbg
-    'bonsai':   [1.577, 3.152, 3.597, 3.434, 3.868, 4.093, 5.669]
+    'bonsai':   [2.421, 2.637, 3.124, 3.935, 4.895, 3.948, 4.119, 4.560, 5.122]
 }
 
 import numpy as np
@@ -42,6 +44,19 @@ import csv
 THROUGHPUT = {}
 for kvstore, latencies in LATENCY.items():
     THROUGHPUT[kvstore] = list(map(lambda x: SIZE * THREADS[x[0]] / x[1] / K, enumerate(latencies)))
+
+MAPPING = {}
+HEADER = ['thread']
+for kvstore, latencies in THROUGHPUT.items():
+    HEADER.append(kvstore)
+    for thread, latency in zip(THREADS, latencies):
+        if thread not in MAPPING:
+            MAPPING[thread] = []
+        MAPPING[thread].append(latency)
+
+print(','.join(HEADER))
+for thread, latencies in MAPPING.items():
+    print(','.join(map(lambda x: str(round(x, 2)), [thread] + latencies)))
 
 xs = THREADS
 
@@ -59,7 +74,7 @@ ax = plt.gca()
 ax.set_xticks(xs)
 ax.set_xticklabels(xs, fontdict=font2)
 
-ytick=[0, 200, 400, 600, 800, 1000, 1200, 1400, 1600]
+ytick=[0, 200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400]
 ax.set_yticks(ytick)
 ax.set_yticklabels(ytick, fontdict=font2)
 
