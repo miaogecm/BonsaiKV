a3f03d89bf1e5ba7ebedf038b2e88291327ec852
hhusjr <gnu_emacs@163.com>
2022-08-17 11:02:58 +0800

    ycsb_load_str

--- evaluation/kvstore/data/final/ycsb_a_int.py
+++ evaluation/kvstore/data/final/ycsb_a_int.py
@@ -13,38 +13,38 @@
 M = 1000000
 MAX = 240 * M
 SIZE = 5.0 * M
-THREADS = [1, 8, 16, 24, 32, 40, 48]
+THREADS = [1, 6, 12, 18, 24, 30, 36, 42, 48]
 LATENCY = {
-    # thread num: 1/8/16/24/32/40/48
+    # thread num: 1/6/12/18/24/30/36/42/48
 
     # bottleneck:
     # Non log-structured: write amplification +
     # Log-structured: contention(BW) + metadata cacheline thrashing(perf L3 cache miss)
 
+    # dm-stripe 2M-Interleave
+    # bottleneck: large SMO overhead, node metadata cacheline thrashing
+    'fastfair': [7.134, 9.799, 11.056, 12.000, 13.202, 21.624, 24.842, 31.675, 45.375],
+
     # Nbg:Nfg = 1:2, at least 1 Nbg
     # dm-stripe 2M-Interleave
     # bottleneck: bloom filter array maintenance, WAL metadata cacheline thrashing, NUMA Awareness
-    'dptree':   [2.818, 8.043, 20.090, 31.996, 44.304, 52.824, 65.354],
+    'dptree':   [4.596, 10.472, 18.069, 25.214, 31.945, 37.688, 43.657, 50.189, 57.191],
+
+    # 1 worker per node (default setting)
+    'pactree':  [9.098, 9.586, 11.796, 12.733, 13.479, 24.368, 32.008, 31.117, 35.446],
 
     # dm-stripe 2M-Interleave
-    # bottleneck: large SMO overhead, node metadata cacheline thrashing
-    'fastfair': [5.083, 8.654, 10.994, 15.638, 26.681, 36.599, 46.999],
+    # LOG_BATCHING enabled, simulates FlatStore log batching (batch size: 512B)
+    'pacman':   [5.698, 7.327, 7.977, 8.516, 8.960, 12.496, 12.899, 13.325, 12.101],
 
     # 1GB memtable, max number=4
     # Enabled 1GB lookup cache (979 MB hash-based, 45 MB second chance)
     # Nbg:Nfg = 1:2, at least 1 Nbg
     # bottleneck: skiplist too high (perf reports high cache-miss rate when lockfree_skiplist::find_position)
-    'listdb':   [6.159, 10.293, 11.202, 12.699, 14.107, 17.873, 20.764],
-
-    # 1 worker per node (default setting)
-    'pactree':  [5.157, 9.146, 11.928, 19.241, 26.778, 35.905, 42.072],
-
-    # dm-stripe 2M-Interleave
-    # LOG_BATCHING enabled, simulates FlatStore log batching (batch size: 512B)
-    'pacman':   [2.156, 4.324, 4.640, 4.969, 8.090, 22.547, 48.724],
+    'listdb':   [7.599, 12.307, 17.301, 23.195, 31.949, 35.298, 46.982, 61.836, 78.329],
 
     # Nbg:Nfg = 1:4, at least 2 Nbg
-    'bonsai':   [1.577, 3.152, 3.597, 3.434, 3.868, 4.093, 5.669]
+    'bonsai':   [3.277, 4.442, 4.915, 5.134, 5.576, 6.556, 7.180, 7.138, 7.289]
 }
 
 import numpy as np
@@ -54,6 +54,19 @@ import csv
 THROUGHPUT = {}
 for kvstore, latencies in LATENCY.items():
     THROUGHPUT[kvstore] = list(map(lambda x: SIZE * THREADS[x[0]] / x[1] / M, enumerate(latencies)))
+
+MAPPING = {}
+HEADER = ['thread']
+for kvstore, latencies in THROUGHPUT.items():
+    HEADER.append(kvstore)
+    for thread, latency in zip(THREADS, latencies):
+        if thread not in MAPPING:
+            MAPPING[thread] = []
+        MAPPING[thread].append(latency)
+
+print(','.join(HEADER))
+for thread, latencies in MAPPING.items():
+    print(','.join(map(lambda x: str(round(x, 2)), [thread] + latencies)))
 
 xs = THREADS
 
@@ -73,7 +86,7 @@ ax = plt.gca()
 ax.set_xticks(xs)
 ax.set_xticklabels(xs, fontdict=font2)
 
-ytick=[0,2,4,6,8,10,12,14,16,18,20,22,24,26,28]
+ytick=[0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32]
 ax.set_yticks(ytick)
 ax.set_yticklabels(ytick, fontdict=font2)
 
