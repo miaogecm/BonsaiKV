ae0d2b12140b3de4f1ad37fc1d0466fc0f9b9eb4
hhusjr <gnu_emacs@163.com>
2022-09-13 20:09:09 +0800

    update ycsb_e_int

--- evaluation/kvstore/data/final/ycsb_e_int_ok.py
+++ evaluation/kvstore/data/final/ycsb_e_int_ok.py
@@ -18,7 +18,7 @@ LATENCY = {
 
     'dptree':   [1.914, 2.222, 2.499, 3.112, 3.903, 25.401, 31.508, 37.077, 42.786],
 
-    'fastfair': [24.754, 29.406, 28.550, 29.932, 32.693, 59.221, 90.554, 119.231, 145.943],
+    'fastfair': [2.848, 3.443, 3.532, 3.599, 4.559, 10.750, 15.031, 19.007, 22.076],
 
     'pacman':   [10.868, 13.169, 14.189, 15.201, 15.248, 33.186, 34.338, 35.470, 36.912],
 
@@ -84,8 +84,8 @@ throughplt.set_xticks(xs)
 throughplt.set_xticklabels(xs, fontdict=font2)
 
 throughplt.set_ylabel("Throughput (M ops/s)", font)
-ytick=[0, 1000, 2000, 3000, 4000, 5000, 6000]
-#throughplt.set_yticks(ytick)
+ytick=[0, 1, 2, 3, 4, 5]
+throughplt.set_yticks(ytick)
 #throughplt.set_yticklabels(ytick, fontdict=font2)
 
 rbwplt.set_ylabel("DIMM Read BW (B/s)", font)
